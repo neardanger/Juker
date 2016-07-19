c974884eea1d5106d892dc10b8ea274eea3fbb5f
Neardanger <steven.magadan@hotmail.com>
2016-07-18 20:07:23 -0700

    added some optimizations

--- api/spotify.js
+++
@@ -1,9 +0,0 @@
-var SpotifyWebApi = require('spotify-web-api-node');
-
-var spotifyApi = new SpotifyWebApi({
-  clientId : 'acbe0fe4c28d442082a36af605de153d',
-  clientSecret : '684a895f030f469f9ca091ae4a352503',
-  redirectUri : 'http://juken.herokuapp.com/callback'
-})
-
-module.exports = spotify
